f0379f0e4884c4268fb71b2aa56f905044641321
sergit <storrellas@gmail.com>
2015-12-04 15:38:13 +0100

    Second commit

--- repricing_django/urls.py
+++ repricing_django/urls.py
@@ -7,4 +7,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    
+    
+    url(r'^dashboard/', include('dashboard.urls')),
 )

efd2cd014413226daabe5be12093b5978288c803
sergit <storrellas@gmail.com>
2015-12-04 17:27:32 +0100

    Last commit

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+import logging
+import json
+logger = logging.getLogger('views')
+
+
 def dashboard(request):
-    #return HttpResponse("return this string")
+
+    
+
+    # POST Method: Used to configure check issuing
+    if request.method == 'POST':
+    
+        # Capture ASIN
+        asin = request.POST.get('asin')        
+        print( 'ASIN: ' + asin )
+
+        
+        # This is a dummy response. Should be beter done
+        data = { 'maximum': 10, 'minimum': 3 }
+        return HttpResponse(json.dumps(data), content_type='application/json')
+
+
+
     return render(request, "dashboard.html")
--- repricing_django/settings.py
+++ repricing_django/settings.py
@@ -119,3 +119,6 @@ STATICFILES_DIRS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+
